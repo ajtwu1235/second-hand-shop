b8e3d2f9bdc6cac6e868d0a7f206714f86cb7a52
ajtwu1235 <75232182+ajtwu1235@users.noreply.github.com>
2022-10-15 14:41:22 +0900

    10-15 aws test

--- src/test/java/com/capstone/skone/SkoneApplicationTests.java
+++ src/test/java/com/capstone/skone/SkoneApplicationTests.java
@@ -1,13 +1,13 @@
-package com.capstone.skone;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SkoneApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.capstone.skone;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SkoneApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
